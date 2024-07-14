8af51655a449189ed472e12a627091c9d38f7b71
azu <azuciao@gmail.com>
2024-07-14 21:00:39 +0900

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    basePath: "/panda-css-nextjs-example",
     reactStrictMode: true,
     experimental: {
         typedRoutes: true
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,28 +3,28 @@ import { css } from "../../styled-system/css";
 import { Container } from "@/app/_layout/Container";
 
 export default function Home() {
-    return (
-        <Container>
-            <ul className={css({
-                display: "flex",
-                flexDirection: "column",
-                gap: "8",
-                padding: "4",
-                margin: "4",
-                listStyle: "none",
+  return (
+    <Container>
+      <ul className={css({
+        display: "flex",
+        flexDirection: "column",
+        gap: "8",
+        padding: "4",
+        margin: "4",
+        listStyle: "none",
 
-            })}>
-                <li>
-                    <Link href="/child" className={css({
-                        color: "blue.700",
-                    })}>test</Link>
-                </li>
-                <li>
-                    <Link href="/child" className={css({
-                        color: "blue.700",
-                    })}>こんにちは</Link>
-                </li>
-            </ul>
-        </Container>
-    );
+      })}>
+        <li>
+          <Link href="/child" className={css({
+            color: "blue.700",
+          })}>test</Link>
+        </li>
+        <li>
+          <Link href="/child" className={css({
+            color: "blue.700",
+          })}>こんにちは</Link>
+        </li>
+      </ul>
+    </Container>
+  );
 }
